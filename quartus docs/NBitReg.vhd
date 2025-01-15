57fd06b1e3481b5a08a5aced3ef52dce362bbd08
R.J. Ockhuijsen <148384806+ockrens@users.noreply.github.com>
2025-01-15 16:30:00 +0100

    heb NBitReg toegevoegd
    er zijn een aantal andere wijzigingen zoals versie en simulatie methode. maar dat komt doordat we ander software versies gebruiken.

---
+++ quartus docs/NBitReg.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity NBitReg is
+	generic (N : integer := 8);
+	port(	D: in std_logic_vector(N-1 downto 0);
+			clk: in std_logic;
+			Q : out std_logic_vector(N-1 downto 0));
+end entity NBitReg;
+
+architecture behaviour of NBitReg is
+begin
+	dflipflop : process(D,clk) is
+	begin
+		if rising_edge(clk) then
+		Q <= D;
+		end if;
+	end process dflipflop;
+end architecture behaviour;
